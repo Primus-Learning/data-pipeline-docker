a1bf5f86bd810855fdb57e66b13bf91bde39951d
Primus Vekuh <primus.vekuh@gmail.com>
2023-02-23 14:19:34 -0500

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -3,11 +3,15 @@ import boto3
 from io import BytesIO
 from datetime import datetime
 
+# Set AWS access key and secret access key
+ACCESS_KEY = 'AKIAT7U2H26L2WRXB67L'
+SECRET_KEY = 'G6lT7LLwLhVb/GpRs68/jMx0Ucq05LCxu7Vxksvh'
+
 # Generate sample data
 data = {
-    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'] * 20000,
-    'age': [25, 32, 18, 41, 29] * 20000,
-    'gender': ['F', 'M', 'F', 'M', 'F'] * 20000
+    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'] * 20,
+    'age': [25, 32, 18, 41, 29] * 20,
+    'gender': ['F', 'M', 'F', 'M', 'F'] * 20
 }
 df = pd.DataFrame(data)
 
@@ -22,7 +26,7 @@ date = now.strftime('%Y-%m-%d')
 timestamp = now.strftime('%Y%m%d%H%M%S')
 
 # Upload to S3 with partitions
-s3 = boto3.resource('s3')
+s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 bucket_name = 'primlearning'
 folder_name = f'upload_date={date}/upload_time={timestamp}'
 key = f'{folder_name}/example.parquet'
